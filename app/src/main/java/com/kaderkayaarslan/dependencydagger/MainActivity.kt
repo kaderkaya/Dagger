ee3ec9d86b3902594144aa41f005dbab87f0e042
kaderkaya <kaderkayaarslan35@gmail.com>
2023-11-03 21:22:30 +0300

    değiştirildi...

--- app/src/main/java/com/kaderkayaarslan/dependencydagger/MainActivity.kt
+++ app/src/main/java/com/kaderkayaarslan/dependencydagger/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         DaggerAppCompanent.builder().build(this)
 
-
         kargo.gonder()
 
         internet.başvuruYap()
